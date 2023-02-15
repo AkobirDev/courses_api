598389da21509e1a3beb8793ec4291d5fffcdd4b
Akobir Tursunov <97350561+AkobirDev@users.noreply.github.com>
2023-02-15 12:23:33 +0500

    Fix debugs

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    avatar = models.ImageField(default='anonimous.png')
+    bio = models.TextField()
+    phone_number = models.PhoneNumberField(_("phone number"), blank=True)
+    is_teacher = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
