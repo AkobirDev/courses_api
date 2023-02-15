915caa8cb93dcabc3d4b622154a8b9b462c06f3a
Akobir Tursunov <97350561+AkobirDev@users.noreply.github.com>
2023-02-15 12:29:00 +0500

    add phone number field

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 class CustomUser(AbstractUser):
     avatar = models.ImageField(default='anonimous.png')
     bio = models.TextField()
-    phone_number = models.PhoneNumberField(_("phone number"), blank=True)
+    phone_number = PhoneNumberField(_("phone number"), blank=True)
     is_teacher = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
